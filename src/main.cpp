596cee0038e3f45ee1c0e58162ff431d6fe0085a
bot-lin <zlin6364@hotmail.com>
2024-07-08 15:58:15 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
 #include <cmath>
 
 #include <string>
